9807eee8b96d0d2176fc5916d55394be94f7fadf
Ayush <ayushsawant2005@gmail.com>
2023-10-26 23:13:15 +0530

    NavBar inserted

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -10,55 +10,50 @@ const NavBar = () => {
 
     return (
         <div> 
-          <header className='header_wrap fixed-top'><nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
+          <div className='header'>
+          <header className='header_wrap fixed-top w-90 p-20'><nav className="navbar bg-dark border-bottom border-body" data-bs-theme="light"> 
+          <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid p-50">
     <a className="navbar-brand" href="#">
-      <img src={logo} alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
-     ARTGuardian
+      <div className='image'>
+              <img src={logo} alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
+     </div>
     </a>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-      <div className='NavBar'>
-      <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        </div>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Why Choose US</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"/></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">Get To Know Us</a>
+        </li>
+      </ul>
     </div>
   </div>
   </nav>
-<button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="offcanvas offcanvas-end " tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Explore</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div className="offcanvas-body">
-        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li className="nav-item">
-            <Link className="nav-link" aria-current="page" href="#">Products</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" href="#">Profile</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" href="#">Connect to Wallet</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" href="#">Get to know Us</Link>
-          </li>
-           
-        </ul>
-        <form className="d-flex mt-3" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
-        </div>
-        </div>
-        </nav>
+  <button className='button'>Connect To Wallet</button>
+
+  </nav>
         </header>
+        </div>
+
         </div>
         
 )
