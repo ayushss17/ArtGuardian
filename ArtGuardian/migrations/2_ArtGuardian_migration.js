8f53583ad9a23a5c15a47988ed0f2d2a7d824efb
Nitish Patil <nitishp1929@gmail.com>
2024-02-25 11:08:16 +0530

    Backend commit

--- ArtGuardian/migrations/2_ArtGuardian_migration.js
+++
@@ -1,5 +0,0 @@
-const PaintingMarketplace = artifacts.require("PaintingMarketplace");
-
-module.exports = function (deployer) {
-    deployer.deploy(PaintingMarketplace);
-};
---
+++ src/backend/mockdatabase.js
@@ -0,0 +1,31 @@
+const mockDatabase = {
+    arts: [
+      {
+        id: 1,
+        title: 'Artwork 1',
+        description: 'Description for Artwork 1',
+        price: 100,
+        artistAddress: '0x123456789abcdef123456789abcdef123456789a',
+      },
+      {
+        id: 2,
+        title: 'Artwork 2',
+        description: 'Description for Artwork 2',
+        price: 200,
+        artistAddress: '0x987654321fedcba987654321fedcba9876543210',
+      },
+      // Add more mock art data as needed
+    ],
+    wallets: [
+      {
+        publicAddress: '0x123456789abcdef123456789abcdef123456789a',
+      },
+      {
+        publicAddress: '0x987654321fedcba987654321fedcba9876543210',
+      },
+      // Add more mock wallet data as needed
+    ],
+  };
+  
+  module.exports = mockDatabase;
+  
